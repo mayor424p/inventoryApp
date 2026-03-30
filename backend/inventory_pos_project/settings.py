4d40d31d56aed2bc90b55be61e7e8e14264d8b5c
Mayowa Abioye <mayowasammy2@gmail.com>
2026-03-30 12:57:32 +0100

    cors_allowed_origins

--- backend/inventory_pos_project/settings.py
+++ backend/inventory_pos_project/settings.py
@@ -154,6 +154,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3002",
+    "http://inventory-app-frontend-git-main-stackplatter1.vercel.app"
 ]
 
 # Internationalization
